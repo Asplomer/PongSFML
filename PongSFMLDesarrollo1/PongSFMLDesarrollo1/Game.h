b5f7a140f3e9d9550165d5d52351d411e21b31c7
unknown <pehuen-testa@hotmail.com>
2018-11-02 12:25:42 -0300

    añadido codigo basico de game

--- PongSFMLDesarrollo1/PongSFMLDesarrollo1/Game.h
+++ PongSFMLDesarrollo1/PongSFMLDesarrollo1/Game.h
@@ -1,7 +1,15 @@
 #ifndef GAME_H
 #define GAME_H
+#include <SFML/Graphics.hpp>
+using namespace sf;
 namespace Pong {
+	const int WIDTH = 20;
+	const int HEIGHT = 70;
+	const int SQUARE = 20;
 
+	void InitGame(int height, int width);
+	void UpdateGame();
+	void DrawGame();
 
 }
 #endif // !GAME_H
---
+++ PongSFMLDesarrollo1/PongSFMLDesarrollo1/SFMLScreen.h
@@ -0,0 +1,9 @@
+#ifndef SFML_SCREEN_H
+#define SFML_SCREEN_H
+#include <iostream>
+using namespace std;
+namespace SFMLcustom {
+
+
+}
+#endif
